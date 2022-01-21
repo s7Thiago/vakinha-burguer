d1deb40d9366996665c0d76e6ea2db998a02ae8c
Thiago Silva <thiago.silva@opah.com.br>
2022-01-21 01:27:44 -0300

    Configurando .env para direcionar variáveis de ambiente Configurando rota auth

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vakinha Burguer
+
+Pagamentos por pix: https://gerencianet.com.br/pix/
+
+DNS: https://www.cloudns.net/
+
+Certificado Digital: https://certbot.eff.org/
--- api/vakinha_burguer_api/bin/server.dart
+++ api/vakinha_burguer_api/bin/server.dart
@@ -3,24 +3,20 @@ import 'dart:io';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:dotenv/dotenv.dart' show load;
+import 'package:vakinha_burguer_api/app/core/modules/auth/auth_controller.dart';
 
 // Configure routes.
-final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
-
-Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
-}
-
-Response _echoHandler(Request request) {
-  final message = params(request, 'message');
-  return Response.ok('$message\n');
-}
+final _router = Router()..mount('/auth/', AuthController().router);
 
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
+
+  // Responsável por carregar as configurações no .env
+  // Chamando somente assim, o considera-se que o arquivo .env está no
+  // diretório raiz do projeto e se chama ".env"
+  load();
 
   // Configure a pipeline that logs requests.
   final _handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
